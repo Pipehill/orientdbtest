7910514e0accb5b0bcba2e95df31d91ac183a4e1
Stian Rørlien <stian.rorlien@bouvet.no>
2020-01-02 09:48:02 +0100

    Initial commit

---
+++ src/main/java/no/bouvet/stian/orientdbtest/OrientdbtestApplication.java
@@ -0,0 +1,48 @@
+package no.bouvet.stian.orientdbtest;
+
+import com.google.gson.Gson;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.sql.executor.OResult;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class OrientdbtestApplication {
+
+    @RequestMapping("/")
+    public String home() {
+
+        System.out.println("Returning data...");
+
+        List<String> toReturn = new ArrayList();
+
+        OrientDB orientDB = new OrientDB("remote:db", OrientDBConfig.defaultConfig());
+        ODatabaseDocument db = orientDB.open("mydb","root", "root");
+
+        String statement = "SELECT FROM Bruker";
+        OResultSet rs = db.query(statement);
+        while(rs.hasNext()){
+            OResult row = rs.next();
+            toReturn.add(row.getProperty("Navn"));
+        }
+        rs.close();
+
+        return new Gson().toJson(toReturn);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientdbtestApplication.class, args);
+
+        System.out.println("Started...");
+        System.out.println("Listening...");
+    }
+}
---
+++ src/test/java/no/bouvet/stian/orientdbtest/OrientdbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package no.bouvet.stian.orientdbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientdbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
